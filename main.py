ac264098f32848d134918acedf5d14ce637c9a89
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-14 02:02:53 +0700

    Share Annotated dependencies

--- main.py
+++ main.py
@@ -9,10 +9,24 @@ app = FastAPI()
 async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
   return {"q": q, "skip": skip, "limit": limit}
 
+"""
 @app.get("/items")
 async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
   return commons
 
 @app.get("/users")
 async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
+  return commons
+"""
+
+# Share Annotated dependencies
+
+CommonDepends = Annotated[dict, Depends(common_parameters)]
+
+@app.get("/items")
+async def read_items(commons: CommonDepends):
+  return commons
+
+@app.get("/users")
+async def read_users(commons: CommonDepends):
   return commons
